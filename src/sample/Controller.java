c1e51abf3d6dfa13bc4e45d88d87cca89fe00653
torla <marcotorlaschi95@gmail.com>
2019-04-03 18:48:15 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class Controller {
+
+  public ListView veicoliList;
+
+  public void load(ActionEvent actionEvent) {
+    //todo load dei dati
+
+    //lista a cui aggiungere ciò che si vuole mostrare nella lista
+    ArrayList<String> list = new ArrayList<>();
+    list.add("a");
+    list.add("a2");
+    ObservableList<String> observableList = FXCollections.observableList(list);
+    veicoliList.setItems(observableList);
+  }
+}
