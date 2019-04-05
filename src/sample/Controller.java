a3dd7e81bfa5e713ae7b866b68ff1ab2aca31df3
torla <marcotorlaschi95@gmail.com>
2019-04-05 15:05:21 +0200

    fix

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,7 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
-import javafx.scene.control.Button;
+
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -29,8 +29,8 @@ public class Controller {
   public TextField motoTempi;
   public TextField motoRapporti;
 
-  public void load(ActionEvent actionEvent) {
-    //todo load dei dati
+  public void save(ActionEvent actionEvent) {
+    //todo save dei dati
 
     //lista a cui aggiungere ciò che si vuole mostrare nella lista
     ArrayList<String> list = new ArrayList<>();
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,11 +8,12 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Veicoli");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1200, 400));
         primaryStage.show();
     }
 
